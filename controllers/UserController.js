e838daebdb866bdb58ca399298477eeef200e2bf
DESKTOP-11PTO7M\Xavi <fdvsfvvd>
2024-11-19 17:11:36 +0100

    Crear y mostrar usuarios

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,1 +1,32 @@
 const { User } = require("../models/index")//importar modelo
+
+const UserController = {
+    async create(req, res) {
+        try {
+            const user = await User.create(req.body)
+            res.status(201).send({ message: "User created", user })
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ message: "ERROR", error })
+        }
+    },
+    async getAll(req, res) {
+        try {
+            const users = await User.findAll({
+                // // include:[Post]
+                // include:{
+                //     model:Post,
+                //     attributes:["title","content"]
+                // }
+
+            })
+            res.status(200).send(users)
+        } catch (error) {
+            console.error(error);
+            res.status(500).send({ message: "ERROR", error })
+        }
+    }
+
+};
+
+module.exports = UserController;
--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = 3000
 
 app.use(express.json())//parsea body si no ponemos esto req.body es undefined
 
-// app.use("/users",require("./routes/users.js"))
+app.use("/users",require("./routes/users.js"))
 // app.use("/posts",require("./routes/posts.js"))
 
 app.listen(PORT,()=> console.log("Servidor levantado en el puerto " + PORT))
