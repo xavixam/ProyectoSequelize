7012e7d34e631a06c79c19672c6885d76e641a7a
DESKTOP-11PTO7M\Xavi <fdvsfvvd>
2024-11-19 16:21:13 +0100

    Modelos añadidos e importados

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express")
+const app = express()
+const PORT = 3000
+
+app.use(express.json())//parsea body si no ponemos esto req.body es undefined
+
+// app.use("/users",require("./routes/users.js"))
+// app.use("/posts",require("./routes/posts.js"))
+
+app.listen(PORT,()=> console.log("Servidor levantado en el puerto " + PORT))
